c65b0dabc8d3bb4851e7d7391f37ace825cf5a53
wub <1422110843@qq.com>
2020-03-16 16:24:09 +0800

    Stream流和Springboot-admin

--- src/main/java/com/cn/mysix/MysixApplication.java
+++ src/main/java/com/cn/mysix/MysixApplication.java
@@ -1,5 +1,6 @@
 package com.cn.mysix;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
+@EnableAdminServer
 @MapperScan("com.cn.mysix.dao")
 @SpringBootApplication
 public class MysixApplication extends SpringBootServletInitializer {
--- src/main/java/com/cn/mysix/config/RabbitMq/SendMessageRabbit.java
+++ src/main/java/com/cn/mysix/config/RabbitMq/SendMessageRabbit.java
@@ -1,38 +1,38 @@
-package com.cn.mysix.config.RabbitMq;
-
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * @ClassName SendMessageRabbit
- * @Description SendMessageRabbit
- * @Author wub
- * @Date 2020/3/13 13:57
- * @Version 1.0
- */
-
-@RestController
-public class SendMessageRabbit {
-
-    @Autowired
-    RabbitTemplate rabbitTemplate;
-
-
-//    @GetMapping("/sendRabbitmq")
-    public String sendRabbitmq(){
-
-        Map<String,Object> map=new HashMap<>();
-        map.put("mesID","rabbitMQ");
-        map.put("mescle","答案");
-
-        rabbitTemplate.convertAndSend("TestDirectExchange","TestDirectRouting",map);
-
-        return "发送";
-
-    }
-}
+//package com.cn.mysix.config.RabbitMq;
+//
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+///**
+// * @ClassName SendMessageRabbit
+// * @Description SendMessageRabbit
+// * @Author wub
+// * @Date 2020/3/13 13:57
+// * @Version 1.0
+// */
+//
+//@RestController
+//public class SendMessageRabbit {
+//
+//    @Autowired
+//    RabbitTemplate rabbitTemplate;
+//
+//
+////    @GetMapping("/sendRabbitmq")
+//    public String sendRabbitmq(){
+//
+//        Map<String,Object> map=new HashMap<>();
+//        map.put("mesID","rabbitMQ");
+//        map.put("mescle","答案");
+//
+//        rabbitTemplate.convertAndSend("TestDirectExchange","TestDirectRouting",map);
+//
+//        return "发送";
+//
+//    }
+//}
